014a1cf9652b4ee1715876e34141edff088dbf5a
sanikajori <sanikajori28@gmail.com>
2024-08-16 12:51:59 +0530

    numberguessinggame.cpp

--- Number Guessing Game/numberguessinggame.cpp
+++ Number Guessing Game/numberguessinggame.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include  <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand ((unsigned int) time (NULL));
+
+    int number = rand () % 100 + 1;   // 0-99 + 1 -> 1-100
+
+    int guess = 0;
+
+    do
+    {
+        cout << "Enter Guess (1-100): ";
+        cin >> guess;
+
+        if ( guess > number)
+           cout << "Guess lower!" << endl;
+        else if (guess < number)
+           cout << "Guess Higher!" << endl;
+        else
+           cout << "You Won!" << endl;
+
+    } while ( guess != number);
+
+    return 0;
+}
 
