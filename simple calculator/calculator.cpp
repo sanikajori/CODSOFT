e1df303d385434e2d067306b6aa81804222ada79
sanikajori <sanikajori28@gmail.com>
2024-08-16 12:14:32 +0530

    calculator.cpp

--- simple calculator/calculator.cpp
+++ simple calculator/calculator.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    double num1, num2;
+    char op;
+
+    cout << "Enter the operator ( + - * / ) : ";
+    cin >> op;
+
+    cout << "Enter the two numbers one by one : ";
+    cin >> num1 >> num2;
+
+    switch ( op )
+    {
+     case '+':
+     cout << num1 << op << num2 << " = " << num1 + num2 << endl;
+     break;
+
+     case '-':
+     cout << num1 << op << num2 <<  " = " << num1 - num2 << endl;
+     break;
+
+     case '*':
+     cout << num1 << op << num2 << " = " <<  num1 * num2 << endl;
+     break;
+
+     case '/':
+       if (num2 == 0.0)
+       {
+           cout << "Divide by zero Situation" << endl;
+       }
+       else
+       {
+        cout << num1 << op << num2 << " = " <<  num1 / num2 << endl;
+       }
+       break;
+
+       default:
+       cout << "Invalid Operator" << endl;
+    }
+}
